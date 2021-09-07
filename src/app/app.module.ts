a5d89262568e6a4ffd5ac18005380935a5b4d451
Cgrubick <ccg0015@mix.wvu.edu>
2021-09-07 18:16:16 -0400

    footer9/7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FootComponent } from './footers/foot.component';
 import { TopNavBarComponent } from './headers/top-nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent
+    TopNavBarComponent,
+    FootComponent
   ],
   imports: [
     BrowserModule
