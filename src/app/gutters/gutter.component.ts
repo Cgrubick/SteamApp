170ded29e30e86b3c265c22a5ee800b816bc8459
ccg0015 <ccg0015@mix.wvu.edu>
2021-09-13 15:55:24 -0400

    gutters

---
+++ src/app/gutters/gutter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-gutter',
+    templateUrl: 'gutter.component.html',
+    styleUrls: ['gutter.component.css']
+})
+export class GutterComponent {
+
+}
